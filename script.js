503d43faf92fab4501229b8260b9f78e7693925a
davud <davudgorqi@gmail.com>
2022-12-31 21:53:09 +0100

    revamp

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+updateProjectsInfo();
+updateSkills();
+
 const navbarLinks = document.querySelectorAll(".navbar-menu-link");
 
 navbarLinks.forEach(navbarLink => {
@@ -15,3 +18,45 @@ navbarLinks.forEach(navbarLink => {
         });
     });
 });
+
+async function updateProjectsInfo() {
+    const res = await fetch("./projects.json");
+    const { data: projects } = await res.json();
+
+    const projectsContainer = document.querySelector("#projects-container");
+    projects.forEach(project => {
+        const projectElement = document.createElement("div");
+        projectElement.classList.add("project-card");
+        projectElement.innerHTML = `
+            <div class="project-header">
+                <h2 class="project-title">${project.name}</h2>
+                <div class="project-card-image">
+                    <img src="${project.cover}" alt="not found" />
+                </div>
+            </div>
+            <span class="project-description">${project.description}</span>
+        `;
+
+        projectsContainer.appendChild(projectElement);
+    });
+}
+
+async function updateSkills() {
+    const res = await fetch("./skills.json");
+    const { data: skills } = await res.json();
+
+    const skillsContainer = document.querySelector("#skills-container");
+    skills.forEach(skill => {
+        const skillElement = document.createElement("div");
+        skillElement.classList.add("skill-card");
+        skillElement.innerHTML = `
+            <img
+                class="skill-logo"
+                src="${skill.image}"
+                alt="not found"
+            />
+        `;
+
+        skillsContainer.appendChild(skillElement);
+    });
+}
